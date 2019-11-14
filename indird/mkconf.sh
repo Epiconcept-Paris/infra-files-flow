93f5b1aca11844dfc3676edd57e44cb46cd5aeb2
Christophe de Traversay <epiconcept@traversay.com>
2019-11-14 08:50:52 +0100

    Update indird/indird.conf

--- indird/mkconf.sh
+++ indird/mkconf.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-
+#
+#	mkconf.sh - Output local indird.conf for tests
+#	DO NOT use in production (if you could) !
+#
 Prg=`basename $0`
 test "$1" || { echo "Usage: $Prg <host>" >&2; exit 1; }
-yaml2json ../indird.yml | jq .hosts.$1.confs | sed -e '1i\
-#\
-#	indird.conf\
-#' \
--e 's/^ \{16\}/\t\t\t\t/' \
--e 's/^ \{14\}/\t\t\t    /' \
--e 's/^ \{12\}/\t\t\t/' \
--e 's/^ \{10\}/\t\t    /' \
--e 's/^ \{8\}/\t\t/' \
--e 's/^ \{6\}/\t    /' \
--e 's/^ \{4\}/\t/' \
--e 's/^ \{2\}/    /'
+yaml2json ../indird.yml | jq .hosts.$1.confs | sed -e "1i\\
+#\\
+#	indird.conf for $1\\
+#" -e 's/  /    /g' | unexpand	# WARNING: will also unexpand in values !
