cbb2c34e98288a71aeb57155161382ed49390c3d
Cédric Girard <c.girard@epiconcept.fr>
2025-10-07 15:18:00 +0200

    ajout vérification que deux flux ne traitent pas le même dossier

--- ansible/files/normed_indird.sh
+++ ansible/files/normed_indird.sh
@@ -6,3 +6,8 @@ IFS=$'\n'
 for i in $($ABS/../indirdctl check); do 
 	echo "SERVEUR;serveur;services;SERVICE_DOWN;service $i down"
 done
+
+IFS=$'\n'
+for i in $(grep cmd /etc/indird.conf |grep -oe '/space/applisdata.*[$]' |sort |uniq -c | awk '$1 != 1'); do 
+	echo "SERVEUR;serveur;services;INDIRD_CONFLIT;multiples occurences d'un chemin de traitement: $i "
+done	
